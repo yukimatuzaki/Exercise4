2efabb44c321369f42bcf3c05fd2989492a665fd
yukimatuzaki <s1280057@u-aizu.ac.jp>
2021-07-06 09:09:58 +0900

    Initial revision

---
+++ diceGame.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+
+
+class diceGame {
+    public static void main(String args[]) {
+      Random rand = new Random();
+      int total =0;
+ //     int num = rand.nextInt(6)+1;
+ System.out.println("Rolling the dice...");
+      for(int i=0;i<2;i++){ 
+        int num = rand.nextInt(6)+1;   
+      System.out.println("Die"+ (i+1)+":"+num);
+        total += num;
+      }
+    System.out.println("Total value:"+total);
+    }
+  }
